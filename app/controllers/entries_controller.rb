974d64c64dd6ca6dbae99b51f2fd616ba95bbcff
sanna <nilsyuan@gmail.com>
2018-12-11 11:50:45 -0500

    Finished.

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,21 @@
+class EntriesController < ApplicationController
+  def index
+    # here we'll define some @instance_variables to store data from the database for the views to use
+    render :index
+  end
+  def show
+    render :show
+  end
+  def new
+    render :new
+  end
+  def create
+    redirect_to entries_url
+  end
+  def edit
+    render :edit
+  end
+  def update
+    redirect_to entry_url(params[:id])
+  end
+end
