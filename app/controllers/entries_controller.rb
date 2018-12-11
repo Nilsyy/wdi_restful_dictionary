54557517acad8de2199c9d8fd37198ee38ec252c
sanna <nilsyuan@gmail.com>
2018-12-11 16:01:36 -0500

    DOne.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,24 +1,60 @@
 class EntriesController < ApplicationController
   def create
-    redirect_to entries_url
+    # redirect_to entry_url
+    @entry = Entry.new
+
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      render new_entry_path
+    end
   end
+
   def update
-    redirect_to entry_url(params[:id])
+    # redirect_to entry_url(params[:id])
+    @entry = Entry.find(params[:id])
+
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      render edit_entry_url(@entry)
+    end
   end
+
   def destroy
-    redirect_to entries_url
+    # redirect_to entries_url
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_path
   end
+
   def index
     # here we'll define some @instance_variables to store data from the database for the views to use
-    render :index
+    # render :index
+    @entries = Entry.all
   end
+
   def show
-    render :show
+    # render :show
+    @entry = Entry.find(params[:id])
   end
+
   def new
-    render :new
+    # render :new
+    @entry = Entry.new
   end
+
   def edit
-    render :edit
+    # render :edit
+    @entry = Entry.find(params[:id])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :entries, except: [:destroy]
+  resources :entries
   resources :entries, only: [:index, :show]
 end
